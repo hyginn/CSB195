6fd1746b9db2019f2e8e501163cbf65ae49e0621
hyginn <boris.steipe@utoronto.ca>
2023-10-12 20:06:49 -0400

    V 1.1 - added option to use one-letter codes

--- R/printGCtable.R
+++ R/printGCtable.R
@@ -2,11 +2,15 @@
 #
 # Purpose: Define the function printGCtable() to print alternate tables for
 #          presenting the genetic code.
-# Version: 1.0
+# Version: 1.1
 # Date:    2023-09-25
 # Author:  CSB195 with ChatGPT-4
 #
 #  THIS SCRIPT CAN BE SOURCE()'D WITHOUT SIDE EFFECT TO LOAD THE FUNCTION.
+#
+# Versions:
+#      1.1  Change to work with both one-letter and three-letter codes
+#      1.0  Basic running code
 #
 # ToDo:
 #    - Comment the code
@@ -17,7 +21,7 @@
 #    - Write test
 #
 # Notes:
-#   The code was developed in class, in a ChatGPT dialogue. The dialogue
+#   The v 1.0 code was developed in class, in a ChatGPT dialogue. The dialogue
 #   is here:
 #     https://chat.openai.com/share/53e3b709-7067-4842-b61e-5a7010181f28
 #
@@ -50,10 +54,26 @@ GCDAT <- "data/GeneticCode.csv"
 # ====  FUNCTIONS  =============================================================
 
 # ChatGPT code, unaltered:
-printGCtable <- function(nuc, order, dat) {
+printGCtable <- function(nuc, order, dat, aaColName = "Aaa") {
+  # Parameters:
+  #   nuc: a permutation of {A, C, G, T} in the order they should appear
+  #        in the table
+  #   order: a permutation of {1, 2, 3} corresponding which nucleotide
+  #        should vary in the tables, rows, columns, and blocks, resspectively
+  #   dat: a data frame with the genetic code: required columns are
+  #        "First", "Second", "Third", "Codon" and the column name for the
+  #        encoded amino acid - either "Aaa" (default) or "A".
+
   # Check input consistency
-  if (length(nuc) != 4 | length(order) != 3 | !all(order %in% 1:3)) {
-    stop("Ensure nucleotide order has length 4 and order has three unique values between 1 and 3.")
+  if (length(nuc) != 4 ) {
+    stop("Ensure \"nuc\" has length 4.")
+  }
+  if (length(order) != 3 || !all(order %in% 1:3)) {
+    stop("Ensure \"order\" has three unique values between 1 and 3.")
+  }
+  if (! any(aaColName == colnames(dat))) {
+    stop(sprintf("Columname \"%s\" does not appear in the dataframe \"dat\"",
+                 aaColName))
   }
 
   row_pos <- order[1]
@@ -67,16 +87,17 @@ printGCtable <- function(nuc, order, dat) {
     return(df_row$Third)
   }
 
-  # Create nested loops to go through nucleotide combinations based on the given order
+  # Create nested loops to go through nucleotide combinations based
+  # on the given order
   for (r in nuc) {
     for (cell in nuc) {
       line <- sapply(nuc, function(c) {
         # Find corresponding Codon and AAA based on r, c, and cell
-        matching_row <- dat[extract_val(dat, row_pos) == r &
-                              extract_val(dat, col_pos) == c &
-                              extract_val(dat, cell_pos) == cell, ]
+        matching_row <- dat[extract_val(dat,  row_pos) == r &
+                            extract_val(dat,  col_pos) == c &
+                            extract_val(dat, cell_pos) == cell, ]
 
-        paste0(matching_row$Codon, " ", matching_row$AAA)
+        paste0(matching_row[ , "Codon"], " ", matching_row[ , aaColName])
       })
       cat(paste(line, collapse="\t"), "\n")
     }
@@ -101,8 +122,17 @@ if (FALSE) {
 
 if (FALSE) {
 
+  # Validate:
+
   # This should produce the standard way to print the code:
+  # ... three-letter code
   printGCtable(nuc = c("T", "C", "A", "G"), order = c(1, 2, 3), dat = GCdf)
+
+  # ... one-letter code
+  printGCtable(nuc = c("T", "C", "A", "G"),
+               order = c(1, 2, 3),
+               dat = GCdf,
+               aaColName = "A")
 
 
 }
