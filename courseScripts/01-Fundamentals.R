c83cd950871443b55d0ad2083cde085fd26e39a9
hyginn <boris.steipe@utoronto.ca>
2024-09-18 18:31:31 -0300

    New exploration script for R

---
+++ courseScripts/01-Fundamentals.R
@@ -0,0 +1,70 @@
+# 01-Fundamentals.R
+#
+# Purpose: Basic concepts of R programming
+# Version: 1.0
+# Date:    2024-09-18
+# Author:  boris.steipe@utoronto.ca
+#
+# ToDo:
+# Notes:
+#
+# ==============================================================================
+
+# Copy this file into your "myScripts" folder. Then open the copy for editing.
+
+# Run the command gAIinit(). Then open a session with your favorite generative
+# AI assistant and paste the starting prompt. Suggested follow-on prompts
+# are included below.
+
+
+# ====  BASIC CONCEPTS  ========================================================
+
+# Prompt:  Please give me a one-sentence definition of "<...>".
+
+
+# ===   algorithm
+
+# ===   program
+
+# ===   code
+
+# ===   data
+
+# ===   data type
+
+
+
+# ====  LANGUAGE ELEMENTS   ====================================================
+
+# Prompt:  Tell me more about "<...>" in R.
+
+
+# ===   names
+
+# ===   keywords
+
+# ===   operators
+
+
+# ====  FUNCTIONS   ============================================================
+
+# Prompt:  Can you explain what a function is in R. How does a function get the
+#          values it works on? How can other functions use the results? And
+#          what are "parameters"?
+
+
+
+
+
+# ====  PACKAGES   =============================================================
+
+# Prompt:  Tell me more about packages in R. Why is that important?
+
+
+
+
+
+
+
+
+# [END]
