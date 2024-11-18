0bd5efe6ae59de8d0258d0ac63b3e591234672dc
hyginn <boris.steipe@utoronto.ca>
2024-11-18 14:28:18 -0400

    New script

---
+++ courseScripts/13-publicDatabases.R
@@ -0,0 +1,77 @@
+# tocID <- "/courseScripts/13-publicDatabases.R"
+#
+#
+# Purpose:  Public databases for the annotation of gene features and functions.
+#
+# Version:   0.1
+#
+# Date:     2024-11
+# Author:   Boris Steipe (boris.steipe@utoronto.ca)
+#
+# Versions:
+#           0.1    First code 2024
+#
+# TODO:
+#
+#
+# ==============================================================================
+
+
+#TOC> ==========================================================================
+#TOC>
+#TOC>   Section  Title                                                       Line
+#TOC> ---------------------------------------------------------------------------
+#TOC>   1        Our Example: TP53                                             31
+#TOC>   2        GO - The Gene Ontology                                        36
+#TOC>   3        KEGG - The Kyoto Encyclopedia of Genes and Genomes            43
+#TOC>   4        Reactome - The Pathway database                               48
+#TOC>   5        The UCSC Genome Browser                                       54
+#TOC>   6        Ensembl - Annotated Genomes at the EBI                        60
+#TOC>
+#TOC> ==========================================================================
+
+
+# =    1  Our Example: TP53  ===================================================
+
+# HGNC
+# https://www.genenames.org/data/gene-symbol-report/#!/hgnc_id/HGNC:11998
+
+
+
+
+# =    2  GO - The Gene Ontology  ==============================================
+
+# https://geneontology.org/
+#
+# Note: GO and GOA
+
+
+
+# =    3  KEGG - The Kyoto Encyclopedia of Genes and Genomes  ==================
+
+# https://www.genome.jp/kegg/pathway.html
+
+
+
+# =    4  Reactome - The Pathway database  =====================================
+
+# https://reactome.org/
+
+
+
+# =    5  The UCSC Genome Browser  =============================================
+
+# https://genome.ucsc.edu/
+
+
+
+# =    6  Ensembl - Annotated Genomes at the EBI  ==============================
+
+# https://useast.ensembl.org/index.html
+#
+# Note: BioMart
+
+
+
+
+# [END]
