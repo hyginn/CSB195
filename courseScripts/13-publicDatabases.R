b5ee41c57dfda43e4c69e419051244ae3ab5c638
hyginn <boris.steipe@utoronto.ca>
2024-11-20 18:18:52 -0400

    New Regex material

--- courseScripts/13-publicDatabases.R
+++ courseScripts/13-publicDatabases.R
@@ -18,7 +18,7 @@
 
 
 #TOC> ==========================================================================
-#TOC> 
+#TOC>
 #TOC>   Section  Title                                                       Line
 #TOC> ---------------------------------------------------------------------------
 #TOC>   1        Our Example: TP53                                             35
@@ -28,13 +28,17 @@
 #TOC>   5        Reactome - The Pathway database                               62
 #TOC>   6        The UCSC Genome Browser                                       68
 #TOC>   7        Ensembl - Annotated Genomes at the EBI                        74
-#TOC> 
+#TOC>
 #TOC> ==========================================================================
 
 
 # =    1  Our Example: TP53  ===================================================
 
+# = 1  HGNC
+#
 # HGNC
+# https://www.genenames.org/
+#
 # https://www.genenames.org/data/gene-symbol-report/#!/hgnc_id/HGNC:11998
 
 
@@ -42,32 +46,64 @@
 
 # https://pubmed.ncbi.nlm.nih.gov/
 #
+# Example:
+# https://pubmed.ncbi.nlm.nih.gov/?term=(TP53[title]+OR+p53[title])+AND+review[PT]
+#
 
-
+# A bookmarklet for faster access to licensed sources via the UofT library.
+# javascript:(function(){var url=window.location.href;var re=/\/([\w.]+)\/(.*$)/;var match=url.match(re);var newURL="http://"+match[1]+".myaccess.library.utoronto.ca/"+match[2];window.location.href=newURL;})();void 0
 
 # =    3  GO - The Gene Ontology  ==============================================
 
 # https://geneontology.org/
 #
 # Note: GO and GOA
-
+#
+# GO contains three independent Ontologies (memorize their names):
+#      F: molecular function
+#      C: cellular component
+#      P: biological process
+#
+#
+# Search in GOA:
+#
+# Example:
+# https://amigo.geneontology.org/amigo/search/annotation?q=%22TP53%22
+# - Put TP53 in quotation marks for exact matches.
+# - Filter for Organism: Homo sapiens
+# - Filkter for Ontology (aspect): P for biological process
 
 
 # =    4  KEGG - The Kyoto Encyclopedia of Genes and Genomes  ==================
 
 # https://www.genome.jp/kegg/pathway.html
+#
+# Example:
+# Enter prefix: "hsa", then serch for "TP53" ...
+# https://www.kegg.jp/pathway/map=hsa04110&keyword=tp53
+
 
 
 
 # =    5  Reactome - The Pathway database  =====================================
 
 # https://reactome.org/
+#
+# Example:
+# https://reactome.org/content/detail/R-HSA-69488
+# https://reactome.org/PathwayBrowser/#/R-HSA-69620&PATH=R-HSA-1640170&FLG=UniProt:P04637
+
 
 
 
 # =    6  The UCSC Genome Browser  =============================================
 
 # https://genome.ucsc.edu/
+#
+# Example:
+# https://genome.ucsc.edu/cgi-bin/hgTracks?db=hg38&lastVirtModeType=default&lastVirtModeExtraState=&virtModeType=default&virtMode=0&nonVirtPosition=&position=chr17%3A7668421%2D7687489&hgsid=2382624373_uWNIHeKw7msVY5ZuSH1e3vAJfJA7
+
+# https://genome.ucsc.edu/cgi-bin/hgTracks?db=hg38&position=chr17%3A7668421%2D7687489
 
 
 
@@ -76,7 +112,9 @@
 # https://useast.ensembl.org/index.html
 #
 # Note: BioMart
-
+#
+# Example:
+# https://useast.ensembl.org/Homo_sapiens/Gene/Phenotype?db=core;g=ENSG00000141510;r=17:7661779-7687546
 
 
 
---
+++ courseScripts/14-parsingAnnotations.R
@@ -0,0 +1,108 @@
+# tocID <- "./courseScripts/14-parsingAnnotations.R"
+#
+# Using reglar expressions to parse phenotype annotations from ensembl
+#
+# ==============================================================================
+
+
+#TOC> ==========================================================================
+#TOC> 
+#TOC>   Section  Title                             Line
+#TOC> -------------------------------------------------
+#TOC>   1        Preparations                        19
+#TOC>   2        Processing ensembl Genes            28
+#TOC>   3        Parsing ...                         98
+#TOC> 
+#TOC> ==========================================================================
+
+
+# =    1  Preparations  ========================================================
+
+if (! requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager")
+}
+if (! requireNamespace("biomaRt", quietly = TRUE)) {
+  BiocManager::install("biomaRt")
+}
+
+# =    2  Processing ensembl Genes  ============================================
+
+if (FALSE) {
+  myMart <- biomaRt::useMart("ensembl", dataset="hsapiens_gene_ensembl")
+
+
+  # ====
+  STRINGedges <- readRDS("./data/STRINGedges.rds")
+
+  # Get the unique Ensembl identifiers from this list of interactions:
+  IDs <- unique(c(STRINGedges$a, STRINGedges$b))
+
+  N <- length(IDs)
+
+  myPhenotypes <- data.frame(sym = character(N),
+                             phe = character(N))
+  tStart <- Sys.time()
+  for (i in 1:N) {
+    pBar(i, N) # ... progress bar
+    ID <- IDs[i]
+    tmp <- biomaRt::getBM(filters = "ensembl_peptide_id",
+                          attributes = c("hgnc_symbol",
+                                         "phenotype_description"),
+                          values = ID,
+                          mart = myMart)
+    if (length(tmp$hgnc_symbol) == 0) {
+      myPhenotypes$sym[i] <- NA
+      myPhenotypes$phe[i] <- NA
+    } else {
+      myPhenotypes$sym[i] <- unique(tmp$hgnc_symbol)
+      myPhenotypes$phe[i] <- paste(unique(tmp$phenotype_description),
+                                   collapse = ", ")
+    }
+  }
+  tEnd <- Sys.time()
+  cat(sprintf("%d genes processed. Time taken: %s\n", N, tEnd - tStart))
+  # ... this took about 2 and a half hours.
+
+  # saveRDS(myPhenotypes, "./data/ensemblPhenotypes.rds")
+
+  # To parse the phenotypes, we need to split them apart, and
+  # concatenate them together
+
+  # Let's first remove all rows from the dataframe that have NA values in the
+  # phenotypes. Those are genes whose function has not been well characterized.
+
+  tmp <- myPhenotypes
+  sel <- is.na(tmp$phe) | (tmp$phe == "NA")
+  sum(sel)
+  tmp <- tmp[! sel, ]
+
+  head(tmp)
+
+  # Next we strsplit()
+  tmp <- strsplit(tmp$phe, ", ")
+  tmp[[1]]
+
+  # We can simply use unlist() to turn this into a vector
+  tmp <- unlist(tmp) # 68,823 annotations
+
+  # We could study these further, for example by using table() to find the most
+  # frequently used annotations.
+
+  # Next we use unique() to get our phenotype list:
+  tmp <- unique(tmp)  # 10,252 unique phenotypes.
+
+  # We save those ... and we can start parsing the terms
+  # saveRDS(tmp, "./data/uniquePhenotypes.rds")
+}
+
+# =    3  Parsing ...  =========================================================
+
+if (FALSE) {
+  myPhenotypes <- readRDS("./data/uniquePhenotypes.rds")
+
+  # Now what ... ?
+
+
+}
+
+# [END]
